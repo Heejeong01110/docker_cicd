9f684a89998144714fada77193071e9b3dfd87af
Heejeong01110 <123rkdrkd@naver.com>
2021-11-24 14:00:24 +0900

    Initial commit

---
+++ src/main/java/com/example/docker_auto_test/DockerAutoTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.docker_auto_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerAutoTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DockerAutoTestApplication.class, args);
+  }
+
+}
